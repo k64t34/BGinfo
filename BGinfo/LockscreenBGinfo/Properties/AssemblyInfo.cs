8cdca48932b3097e153776214b5eb34f1fabdc9a
k64t34 <k64t34@gmail.com>
2020-09-11 16:48:19 +0300

    11092020

--- BGinfo/LockscreenBGinfo/Properties/AssemblyInfo.cs
+++ BGinfo/LockscreenBGinfo/Properties/AssemblyInfo.cs
@@ -5,9 +5,13 @@ using System.Runtime.InteropServices;
 using System.Resources;
 
 // General Information
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyTitle("LockScreenBGInfo")]
 [assembly: AssemblyDescription("Set image for Windows  lockscreen for all users")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BGInfo")]
 [assembly: AssemblyCopyright("Skorik (c) 2020")]
@@ -15,7 +19,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
